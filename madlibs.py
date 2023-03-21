056c8acd6f15df059fbfaed8c6cfd616757248f6
Er sitesh chaudhary <127986547+siteshh12@users.noreply.github.com>
2023-03-21 21:47:21 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,7 @@
 adj = input("Adjective:")
-verb1 = input("verb1: ")
-verb2 = input("verb2: ")
-verb3 = input("verb3: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because i love to {verb1}.Coding {verb2} me & coding is my {verb3}!"
 print(madlib)
