b30bf263653176c1851b62cdac721cb3238c1399
Er sitesh chaudhary <127986547+siteshh12@users.noreply.github.com>
2023-03-21 21:44:10 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective:")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+verb3 = input("verb3: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because i love to {verb1}.Coding {verb2} me & coding is my {verb3}!"
