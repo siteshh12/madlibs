2531c46c4d0c2ae5a79b4eaf945d8d6aa1c8eec6
Er sitesh chaudhary <127986547+siteshh12@users.noreply.github.com>
2023-04-05 22:48:59 +0530

    Add files via upload

---
+++ spamdetector.py
@@ -0,0 +1,6 @@
+post= input("enter a post:")
+list = ['limited time offer','click ad','free','100% free']
+if post in list:
+    print("it is a spam message")
+else:
+   print("it is not a spam message")
